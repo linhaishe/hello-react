fdf2c2c37edaaf1692aff25a2531ae3b53842c47
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-02-21 00:02:27 +0800

    finished

--- react_app/src/components/search.jsx
+++ react_app/src/components/search.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Search extends Component {
+  static propTypes = {
+    setSearchName: PropTypes.func.isRequired
+  };
+
+  //接收input内的输入数据
+  search = () => {
+    //得到输入关键字
+    const searchName = this.input.value.trim();
+    if (searchName) {
+      //search
+      this.props.setSearchName(searchName);
+    }
+  };
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search" />
-          <button>Search</button>
+          <input
+            type="text"
+            placeholder="enter the name you search"
+            ref={input => (this.input = input)}
+          />
+          <button onClick={this.search}>Search</button>
         </div>
       </section>
     );
