e6afc7be4e86349e6d2b119844c8af4531ae57e3
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-02-22 23:49:17 +0800

    router 笔记

--- react_router/src/index.js
+++ react_router/src/index.js
@@ -1,19 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter, HashRouter} from 'react-router-dom'
-import App from './components/app'
+import React from "react";
+import ReactDOM from "react-dom";
+// import {render} from 'react-dom'
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./components/app";
 
-import './index.css'
+import "./index.css";
 
 ReactDOM.render(
-  (
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-    /*<HashRouter>
+  //一旦用了router,必须用路由组件包裹起来，用路由组件管理整个应用
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  /*<HashRouter>
       <App />
     </HashRouter>*/
-  ),
-
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
