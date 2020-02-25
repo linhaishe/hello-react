5c415be45ff3456cb3fce14878df222c39c4e9d1
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-02-25 23:36:25 +0800

    react redux中间件

--- react_redux/src/redux/store.js
+++ react_redux/src/redux/store.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import reducers from './reducers'
+import reducers from "./reducers";
 
 // 根据counter函数创建store对象
 export default createStore(
   reducers,
-  composeWithDevTools(applyMiddleware(thunk)) // 应用上异步中间件
-)
+  composeWithDevTools(applyMiddleware(thunk))
+  // 应用上异步中间件
+);
