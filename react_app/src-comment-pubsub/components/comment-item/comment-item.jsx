9566d12ce7da2bf82b3231a2ceb93fafd54f3b07
linhaishe <42030043+linhaishe@users.noreply.github.com>
2020-04-08 20:39:55 +0800

    selflearning pracs

--- react_app/src-comment-pubsub/components/comment-item/comment-item.jsx
+++ react_app/src-comment-pubsub/components/comment-item/comment-item.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import PubSub from "pubsub-js";
 //引入文件和第三方引入空格区分
 
 import "./commentItem.css";
@@ -7,16 +8,20 @@ import "./commentItem.css";
 export default class CommentItem extends Component {
   static propTypes = {
     comment: PropTypes.object.isRequired,
-    deleteComment: PropTypes.array.isRequired,
-    index: PropTypes.number.isRequired,
+    // deleteComment: PropTypes.array.isRequired,
+    index: PropTypes.number.isRequired
   };
 
   handleClick = () => {
-    const { comment, deleteComment, index } = this.props;
+    // deleteComment
+    const { comment, index } = this.props;
     //提示
     if (window.confirm(`确定删除${comment.username}的评论吗`)) {
       //确定后删除
-      deleteComment(index);
+      // deleteComment(index);
+      //发布消息，发生事件的地方才需要发布消息
+      //如果要传送多个数据，则封装成对象
+      PubSub.publish("deleteComment", index);
     }
   };
   render() {
--- react_app/src/components/app/app.jsx
+++ react_app/src/components/app/app.jsx
@@ -1,49 +1,24 @@
 import React, { Component } from "react";
-import PubSub from "pubsub-js";
-
-import CommentAdd from "../comment-add/comment-add";
-import CommentList from "../comment-list/comment-list";
+import CommentAdd from "../../../src/components/comment-add/comment-add";
+import CommentList from "../../../src/components/comment-list/comment-list";
 
 export default class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     comments: [
-  //       { username: "tom", content: "react is good!" },
-  //       { username: "jack", content: "react is so hard!" }
-  //     ]
-  //   };
-  // }
-  //给组件对象添加state属性，默认的原始值是null,组件对象是this.state中的this
   state = {
     comments: [
-      { username: "tom", content: "react is good!" },
-      { username: "jack", content: "react is so hard!" },
+      { username: "tom", content: "react is so hard!" },
+      { username: "jack", content: "react is so easy!" },
     ],
   };
 
-  componentDidMount() {
-    //订阅消息
-    PubSub.subscribe("deleteComment", (msg, index) => {
-      this.deleteComment(index);
-    });
-  }
-  //数据在哪个组件，更新数据的行为就应该定义在在哪个组件
-  //这个函数自己不用，放在app.js内
   addComment = (comment) => {
     const { comments } = this.state;
     comments.unshift(comment);
     this.setState({ comments });
   };
 
-  //delete comment
   deleteComment = (index) => {
     const { comments } = this.state;
-
-    //根据元素下标进行删除
-
     comments.splice(index, 1);
-
     this.setState({ comments });
   };
 
@@ -62,8 +37,7 @@ export default class App extends Component {
         </header>
         <div className="container">
           <CommentAdd addComment={this.addComment} />
-          <CommentList comments={comments} />
-          {/* deleteComment={this.deleteComment} 删除*/}
+          <CommentList comments={comments} deleteComment={this.deleteComment} />
         </div>
       </div>
     );
