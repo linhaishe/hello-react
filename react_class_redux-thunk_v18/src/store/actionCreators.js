1bc83b6615ca72ba7a55df81d26ede240898b783
linhaishe <42030043+linhaishe@users.noreply.github.com>
2022-07-29 17:32:47 +0800

    redux-thunk usaage

---
+++ react_class_redux-thunk_v18/src/store/actionCreators.js
@@ -0,0 +1,31 @@
+import { CHANGE_INPUT_VALUE, INIT_LIST_ACTION, SUBMIT } from "./actionTypes";
+import axios from "axios";
+
+export const getInputChangeAction = (value) => ({
+  type: CHANGE_INPUT_VALUE,
+  value,
+});
+
+export const getAddItemAction = () => ({
+  type: SUBMIT,
+});
+
+export const initialListAction = (data) => ({
+  type: INIT_LIST_ACTION,
+  data,
+});
+
+// 返回的是函数，而不是对象
+export const getTodoList = () => {
+  // return async (dispatch) => {
+  //   const { data } = await axios.get("/api");
+  //   dispatch(initialListAction(data));
+  // };
+
+  return (dispatch) => {
+    axios.get("/api").then((res) => {
+      const data = res.data;
+      dispatch(initialListAction(data));
+    });
+  };
+};
---
+++ react_class_redux-thunk_v18/src/store/actionTypes.js
@@ -0,0 +1,3 @@
+export const CHANGE_INPUT_VALUE = "change_input_value";
+export const SUBMIT = "submit";
+export const INIT_LIST_ACTION = "init_list_action";
